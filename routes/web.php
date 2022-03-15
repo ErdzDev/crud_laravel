1c40db952045dae831f9b17236d8ca48c5f5c306
Eraldo Junior <eraldojunior3@gmail.com>
2022-03-14 22:54:55 -0300

    Colocando autenticação em paginas

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 });
 
-Route::get('/usuarios', 'UsuariosController@usuarios')->name('usuarios');
-Route::get('/usuarios/new', 'UsuariosController@new')->name('new');
-Route::post('/usuarios/add', 'UsuariosController@add')->name('add');
-Route::get('/usuarios/{id}/edit', 'UsuariosController@edit')->name('edit');
-Route::post('/usuarios/{id}/update', 'UsuariosController@update')->name('update');
-Route::delete('/usuarios/{id}/delete', 'UsuariosController@delete')->name('delete');
+Route::get('/usuarios', 'UsuariosController@usuarios')->name('usuarios')->middleware('auth');
+Route::get('/usuarios/new', 'UsuariosController@new')->name('new')->middleware('auth');
+Route::post('/usuarios/add', 'UsuariosController@add')->name('add')->middleware('auth');
+Route::get('/usuarios/{id}/edit', 'UsuariosController@edit')->name('edit')->middleware('auth');
+Route::post('/usuarios/{id}/update', 'UsuariosController@update')->name('update')->middleware('auth');
+Route::delete('/usuarios/{id}/delete', 'UsuariosController@delete')->name('delete')->middleware('auth');
